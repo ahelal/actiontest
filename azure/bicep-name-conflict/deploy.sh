2a5cbca079a6c0d81e7c11e7ca4a842d9b327585
Adham <ahelal@users.noreply.github.com>
2024-08-02 08:37:50 +0200

    fix path

--- azure/bicep-name-conflict/deploy.sh
+++ azure/bicep-name-conflict/deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+set -e
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd)"
 resource_group="bender_test_rg"
+
+cd "${SCRIPT_DIR}"
 
 az deployment group create \
     --resource-group "${resource_group}" \
